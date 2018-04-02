714a2b3642998d218e1ce8454208e3c58118e1bb
wachka06 <wachka06@gmail.com>
2018-04-02 22:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let's talk about $ic}{top.`;
+var conversation = `Let's talk about ${topic}.`;
 
